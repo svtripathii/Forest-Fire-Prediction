0d18fd67de93c0d0be959b82a566ab68bbb5473a
Saurav Tripathi <95672002+svtripathii@users.noreply.github.com>
2024-09-16 11:24:12 +0530

    Updated

--- application.py
+++ application.py
@@ -1,43 +1,42 @@
 import pickle
-from flask import Flask,request,jsonify,render_template 
-import numpy as np 
-import pandas as pd 
+from flask import Flask, request, jsonify, render_template
+import numpy as np
+import pandas as pd
 from sklearn.preprocessing import StandardScaler
 
 application = Flask(__name__)
-app=application
-## import ridge regrssor model and standar scaler pickle 
+app = application
+
+# Import ridge regression model and standard scaler pickle
 ridge_model = pickle.load(open('Models/ridge.pkl', 'rb'))
 standard_scaler = pickle.load(open('Models/scaler.pkl', 'rb'))
 
-
-## Route for homepage 
+# Route for homepage
 @app.route('/')
 def index():
-    return render_template('index.html') 
+    return render_template('index.html')
 
-@app.route('/predictdata',methods=['GET','POST'])
+@app.route('/predictdata', methods=['GET', 'POST'])
 def predict_datapoint():
-    if request.method=='POST':
-        Temperature=float(request.form.get('Temperature'))
-        RH = float(request.form.get('RH'))
-        Ws = float(request.form.get('Ws'))
-        Rain = float(request.form.get('Rain'))
-        FFMC = float(request.form.get('FFMC'))
-        DMC = float(request.form.get('DMC'))
-        ISI = float(request.form.get('ISI'))
+    if request.method == 'POST':
+        Temperature = float(request.form.get('Temperature'))
+        Relative_Humidity = float(request.form.get('Relative Humidity'))
+        Wind_Speed = float(request.form.get('Wind Speed'))
+        Rainfall = float(request.form.get('Rainfall'))
+        FFMC = float(request.form.get('Fine Fuel Moisture Code'))
+        DMC = float(request.form.get('Duff Moisture Code'))
+        ISI = float(request.form.get('Initial Spread Index'))
         Classes = float(request.form.get('Classes'))
         Region = float(request.form.get('Region'))
 
-        new_data_scaled=standard_scaler.transform([[Temperature,RH,Ws,Rain,FFMC,DMC,ISI,Classes,Region]])
-        result=ridge_model.predict(new_data_scaled)
+        # Scaling the input data
+        new_data_scaled = standard_scaler.transform([[Temperature, Relative_Humidity, Wind_Speed, Rainfall, FFMC, DMC, ISI, Classes, Region]])
+        result = ridge_model.predict(new_data_scaled)
 
-        return render_template('home.html',result=result[0])
-        pass
+        return render_template('home.html', result=result[0])
     else:
         return render_template('home.html')
-    
 
 
-if __name__=="__main__":
-    app.run(host="0.0.0.0")
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", debug=True)
